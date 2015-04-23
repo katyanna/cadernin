819086c8a543151fdcbac2a96280cab461445b88
Katyanna Moura <amelie.kn@gmail.com>
2015-04-23 20:01:36 -0300

    Lists notes in index

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -13,7 +13,10 @@ class NotesController < ApplicationController
 		@note = Note.find(params[:id])
 	end
 
-
+	def index
+		@notes = Note.all
+	end
+	
 	private
 	def note_params
 		params.require(:note).permit(:title, :text)
